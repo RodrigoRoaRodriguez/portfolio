2010f77dfa72342e0d9ef74035d3df41297071c9
Rodrigo Roa Rodríguez <rodrigoroarod@gmail.com>
2017-01-16 12:58:48 +0100

    Fix: Add tap-event-plugin

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+// Temporal, needed for onTouchTap, which some components have
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 /**
  * Material-UI components require a theme to be provided. MuiThemeProvider
